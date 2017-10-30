93ce8dde6a86b3d629a082cc02ea7aec72445d00
frontIT <12344@qq.com>
2017-10-30 17:56:56 +0800

    修改爬虫

---
+++ 4-superagent-cheerio简单爬虫/README.md
@@ -0,0 +1,3 @@
+# 注意事项
+
+本文之前是通过superagent 和eventproxy控制(2014)现在自己尝试用axios和promise编写
--- 4-superagent-cheerio简单爬虫/app.js
+++ 4-superagent-cheerio简单爬虫/app.js
@@ -1,15 +1,13 @@
-var express = require('express')
 var cheerio = require('cheerio')
 //链式调用到了极致出名
 var request = require('superagent')
+var axios = require('axios')
 
 // url 模块是 Node.js 标准库里面的
 var url = require('url')
 
-// 调用 express 实例，它是一个函数，不带参数调用时，会返回一个 express 实例，将这个变量赋予 app 变量。
-var app = express()
-
 var cnodeUrl = 'https://cnodejs.org/'
+var topicUrls = []
 request
   .get(cnodeUrl)
   .end(function (err, sres) {
@@ -17,7 +15,6 @@ request
           return next(err)
       }
       var $ = cheerio.load(sres.text)
-      var topicUrls = []
       $('#topic_list .topic_title').each(function (idx, element) {
           var $element = $(element)
           // $element.attr('href') 本来的样子是 /topic/542acd7d5d28233425538b04
@@ -27,6 +24,36 @@ request
           var href = url.resolve(cnodeUrl, $element.attr('href'))
           topicUrls.push(href)
       })
-
-      console.log(topicUrls)
+      topicUrls.forEach(function (topicUrl) {
+          /*request.get(topicUrl)
+            .end(function (err, res) {
+                console.log('fetch ' + topicUrl + ' successful')
+            })*/
+      })
   })
+/*
+function getTopic (topicUrl) {
+    return new Promise(resolve, reject) {
+        request.get(topicUrl)
+          .end(function (err, res) {
+              console.log('fetch ' + topicUrl + ' successful')
+          })
+    }
+}*/
+
+// 不可.
+/*
+axios.get(cnodeUrl).then((err, res) =>{
+    var $ = cheerio.load(res.text)
+    var topicUrls = []
+    $('#topic_list .topic_title').each(function (idx, element) {
+        var $element = $(element)
+        var href = url.resolve(cnodeUrl, $element.attr('href'))
+        topicUrls.push(href)
+    })
+    console.log(topicUrls)
+}).catch((err) =>{
+    console.log(err)
+})
+*/
+
