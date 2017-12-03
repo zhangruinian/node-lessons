7bec9f011305362eb185508a72ef8ab575ac2aa9
“frontIT” <zhangruinian3@gami.com>
2017-12-03 15:46:41 +0800

    增加理解异步 %s

--- node实战-tj/fs读取json文件系统文件.js
+++ node实战-tj/fs读取json文件系统文件.js
@@ -1,6 +1,8 @@
 let fs = require('fs')
 
+// 默认都是异步的 除非加上sync前缀
 fs.readFile('../package.json', function (err, data) {
     // 读取的data是buffer类型,tostring方法即可
     console.log(JSON.parse(data.toString()))
 })
+console.log('%s %s', 11, 'a')
